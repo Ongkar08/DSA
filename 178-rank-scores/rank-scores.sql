a52e48e5bfa11f3a0d0f8543073dca3b25e33349
Ongkar08 <ongkarpal@gmail.com>
2026-05-29 12:10:05 +0530

    Time: 468 ms (13.64%) | Memory: 0B (100.00%) - LeetSync

--- 178-rank-scores/rank-scores.sql
+++ 178-rank-scores/rank-scores.sql
@@ -1,26 +1,12 @@
 # Write your MySQL query statement below
-with recursive ranks as (
+with recursive ranks as (select max(score) as score,1 as rnk from scores
+union all
     select
-        max(score) as score,
-        1 as rnk
-    from scores
-
-    union all
-
-    select
-        (
-            select max(score)
-            from scores
-            where score < r.score
-        ),
-        rnk + 1
+        (select max(score) from scores where score < r.score), rnk + 1
     from ranks r
     where score is not null
 )
-
-select
-    s.score,
-    r.rnk as `rank`
+select s.score, r.rnk as `rank`
 from scores s
 join ranks r
 on s.score = r.score
